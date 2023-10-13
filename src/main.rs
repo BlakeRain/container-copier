6ea4e273fad72397c5394dd000bebb1e4e16b7a0
Blake Rain <blake.rain@blakerain.com>
2023-10-13 15:32:02 +0100

    feat: add event specification in configuration (closes #1)

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use std::{
 use clap::Parser;
 use futures_util::StreamExt;
 use inotify::{Inotify, WatchDescriptor, WatchMask};
-use lazy_static::lazy_static;
 use serde::Deserialize;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -27,12 +26,85 @@ struct Args {
     config: PathBuf,
 }
 
+#[derive(Debug, Copy, Clone, Deserialize)]
+enum NotifyEvent {
+    #[serde(alias = "ACCESS")]
+    Access,
+    #[serde(alias = "ATTRIB")]
+    Attrib,
+    #[serde(alias = "CLOSE_WRITE")]
+    CloseWrite,
+    #[serde(alias = "CLOSE_NOWRITE")]
+    CloseNoWrite,
+    #[serde(alias = "CREATE")]
+    Create,
+    #[serde(alias = "DELETE")]
+    Delete,
+    #[serde(alias = "DELETE_SELF")]
+    DeleteSelf,
+    #[serde(alias = "MODIFY")]
+    Modify,
+    #[serde(alias = "MOVE_SELF")]
+    MoveSelf,
+    #[serde(alias = "MOVED_FROM")]
+    MovedFrom,
+    #[serde(alias = "MOVED_TO")]
+    MovedTo,
+    #[serde(alias = "OPEN")]
+    Open,
+    #[serde(alias = "ALL")]
+    All,
+    #[serde(alias = "MOVE")]
+    Move,
+    #[serde(alias = "CLOSE")]
+    Close,
+    #[serde(alias = "DONT_FOLLOW")]
+    DontFollow,
+    #[serde(alias = "EXCL_UNLINK")]
+    ExclUnlink,
+    #[serde(alias = "ONESHOT")]
+    Oneshot,
+}
+
+impl From<NotifyEvent> for WatchMask {
+    fn from(value: NotifyEvent) -> Self {
+        match value {
+            NotifyEvent::Access => WatchMask::ACCESS,
+            NotifyEvent::Attrib => WatchMask::ATTRIB,
+            NotifyEvent::CloseWrite => WatchMask::CLOSE_WRITE,
+            NotifyEvent::CloseNoWrite => WatchMask::CLOSE_NOWRITE,
+            NotifyEvent::Create => WatchMask::CREATE,
+            NotifyEvent::Delete => WatchMask::DELETE,
+            NotifyEvent::DeleteSelf => WatchMask::DELETE_SELF,
+            NotifyEvent::Modify => WatchMask::MODIFY,
+            NotifyEvent::MoveSelf => WatchMask::MOVE_SELF,
+            NotifyEvent::MovedFrom => WatchMask::MOVED_FROM,
+            NotifyEvent::MovedTo => WatchMask::MOVED_TO,
+            NotifyEvent::Open => WatchMask::OPEN,
+            NotifyEvent::All => WatchMask::ALL_EVENTS,
+            NotifyEvent::Move => WatchMask::MOVE,
+            NotifyEvent::Close => WatchMask::CLOSE,
+            NotifyEvent::DontFollow => WatchMask::DONT_FOLLOW,
+            NotifyEvent::ExclUnlink => WatchMask::EXCL_UNLINK,
+            NotifyEvent::Oneshot => WatchMask::ONESHOT,
+        }
+    }
+}
+
 #[derive(Deserialize)]
 struct Config {
     copysets: Vec<Copyset>,
 }
 
 impl Config {
+    fn default_events() -> Vec<NotifyEvent> {
+        vec![
+            NotifyEvent::Create,
+            NotifyEvent::Delete,
+            NotifyEvent::Modify,
+        ]
+    }
+
     // Load TOML config from the given path.
     fn load<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {
         let path = path.as_ref();
@@ -68,30 +140,31 @@ impl Config {
 #[derive(Deserialize)]
 struct Copyset {
     name: String,
+    #[serde(default = "Config::default_events")]
+    events: Vec<NotifyEvent>,
     source: PathBuf,
-    target: Option<PathBuf>,
+    target: PathBuf,
     targets: Vec<Target>,
-}
-
-lazy_static! {
-    static ref WATCH_MASK: WatchMask = WatchMask::CREATE | WatchMask::DELETE | WatchMask::MODIFY;
 }
 
 impl Copyset {
     async fn add_to_watch(&self, env: &mut Env) -> std::io::Result<()> {
-        tracing::info!("Adding watch for copyset {:?}", self.name);
-        tracing::info!("  Source: {:?}", self.source);
-        tracing::info!("  Target: {:?}", self.target);
+        tracing::info!(source = ?self.source, target = ?self.target,
+                       "Adding watch for copyset {:?}", self.name);
 
-        for target in &self.targets {
-            tracing::info!("  {:?} -> {:?}", target.source, target.target);
-            let source = self.source.join(&target.source);
+        for target_spec in &self.targets {
+            tracing::info!(target_spec = ?target_spec,
+                           "Adding target: {:?}", target_spec.source);
+
+            // Append the target's paths (source and target) to the corresponding paths in the
+            // copyset. Note that if the 'target' path is not specified, we assume the value of the
+            // 'source' path.
+            let source = self.source.join(&target_spec.source);
             let target = self
                 .target
-                .as_ref()
-                .unwrap_or(&self.source)
-                .join(&target.target);
+                .join(target_spec.target.as_ref().unwrap_or(&target_spec.source));
 
+            // Check if the target file exists.
             let target_exists = target
                 .try_exists()
                 .map_err(|err| {
@@ -100,6 +173,8 @@ impl Copyset {
                 })
                 .unwrap_or(false);
 
+            // Create the 'ResolvedTarget' and then check if we need to make an initial copy of the
+            // file.
             let target = ResolvedTarget::new(source.clone(), target);
             if source.is_file() && !target_exists {
                 tracing::info!("  Target does not exist; copying");
@@ -109,14 +184,23 @@ impl Copyset {
                 })?;
             }
 
-            let wd = env
-                .notify
-                .watches()
-                .add(&source, *WATCH_MASK)
-                .map_err(|err| {
-                    tracing::error!("  Failed to add watch: {err:?}");
-                    err
-                })?;
+            // If the target specifies it's own events then we want to use those events instead of
+            // those of the copyset; otherwise the events are those of the copyset.
+            let events: WatchMask = if let Some(events) = &target_spec.events {
+                events.iter()
+            } else {
+                self.events.iter()
+            }
+            .copied()
+            .map(WatchMask::from)
+            .collect();
+
+            // Add the source with the computed evens to inotify. The descriptor that we get back is
+            // stored against our 'ResolvedTarget' in the environment.
+            let wd = env.notify.watches().add(&source, events).map_err(|err| {
+                tracing::error!("  Failed to add watch: {err:?}");
+                err
+            })?;
 
             env.targets.insert(wd, target);
         }
@@ -125,10 +209,11 @@ impl Copyset {
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Target {
+    events: Option<Vec<NotifyEvent>>,
     source: PathBuf,
-    target: PathBuf,
+    target: Option<PathBuf>,
 }
 
 struct Env {
